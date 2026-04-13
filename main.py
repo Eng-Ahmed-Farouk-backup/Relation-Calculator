65a820c0a74e4478fb5c095acdbac0169ce74a2b
Eng-Ahmed-Farouk-backup <ahmed.farouk@adapt-eg.org>
2026-04-13 15:33:00 +0200

    patched syntax error
    patched an error where i forgot to add a closing bracket

--- main.py
+++ main.py
@@ -14,30 +14,30 @@ app.add_middleware(
 @app.get("/relation")
 def relation(relation: str, X: str, Y: str): 
     X = set(map(float,X.split(","))) if ":" not in X else set(range(int(X.split(":")[0]),int(X.split(":")[1])+1))
-    Y = set(map(float,Y.split(","))) if ":" not in Y else set(range(int(Y.split(":"[0]),int(Y.split(":")[1])+1)))
+    Y = set(map(float,Y.split(","))) if ":" not in Y else set(range(int(Y.split(":")[0]),int(Y.split(":")[1])+1))
     return {"relation":calculate_relation(relation,X,Y)}
 
 @app.get("/domain")
 def domain(relation: str, X: str, Y: str):
     X = set(map(float,X.split(","))) if ":" not in X else set(range(int(X.split(":")[0]),int(X.split(":")[1])+1))
-    Y = set(map(float,Y.split(","))) if ":" not in Y else set(range(int(Y.split(":"[0]),int(Y.split(":")[1])+1)))
+    Y = set(map(float,Y.split(","))) if ":" not in Y else set(range(int(Y.split(":")[0]),int(Y.split(":")[1])+1))
     return {"domain":calculate_domain(relation,X,Y)}
 
 @app.get("/range")
 def range_(relation: str, X: str, Y: str):
     X = set(map(float,X.split(","))) if ":" not in X else set(range(int(X.split(":")[0]),int(X.split(":")[1])+1))
-    Y = set(map(float,Y.split(","))) if ":" not in Y else set(range(int(Y.split(":"[0]),int(Y.split(":")[1])+1)))
+    Y = set(map(float,Y.split(","))) if ":" not in Y else set(range(int(Y.split(":")[0]),int(Y.split(":")[1])+1))
     return {"range":calculate_range(relation,X,Y)}
 
 @app.get("/cartesian_product")
 def cartesian_product(X: str, Y: str):
     X = set(map(float,X.split(","))) if ":" not in X else set(range(int(X.split(":")[0]),int(X.split(":")[1])+1))
-    Y = set(map(float,Y.split(","))) if ":" not in Y else set(range(int(Y.split(":"[0]),int(Y.split(":")[1])+1)))
+    Y = set(map(float,Y.split(","))) if ":" not in Y else set(range(int(Y.split(":")[0]),int(Y.split(":")[1])+1))
     return {"product":calculate_cartesian_product(X,Y)}
 
 @app.get("/is_function")
 def is_function_(relation: str, X: str, Y: str):
     X = set(map(float,X.split(","))) if ":" not in X else set(range(int(X.split(":")[0]),int(X.split(":")[1])+1))
-    Y = set(map(float,Y.split(","))) if ":" not in Y else set(range(int(Y.split(":"[0]),int(Y.split(":")[1])+1)))
+    Y = set(map(float,Y.split(","))) if ":" not in Y else set(range(int(Y.split(":")[0]),int(Y.split(":")[1])+1))
     return {"is_function":is_function(relation,X,Y)}
 
